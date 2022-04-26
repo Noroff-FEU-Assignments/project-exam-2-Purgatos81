f3d4d9047675e88ceb9cf7ed719054e0528ff4fe
John HC <69842878+Purgatos81@users.noreply.github.com>
2022-04-26 15:25:29 +0200

    edit: editet to targethotel

--- src/components/hotels/HotelTarget.js
+++ src/components/hotels/HotelTarget.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function TargetHotel({ id, Name, Description }) {
+function TargetHotel({ id, Name, Description, Hotelimage }) {
     return (
         <Link to={`details/${id}`}>
             <h4>{Name}</h4>
