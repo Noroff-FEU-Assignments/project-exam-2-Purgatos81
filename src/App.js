c36d74bdf945def3cc47b3ac4fd8df67d31e1927
John HC <69842878+Purgatos81@users.noreply.github.com>
2022-05-01 13:35:04 +0200

    edit:added admin route protection

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from './components/layouts/Footer';
 import { AuthProvider } from './components/context/AuthContext';
 import GlobalStyle from './components/styles/GlobalStyles';
 
+
 function App() {
   return (
     <>
@@ -22,6 +23,7 @@ function App() {
             <Route path='contact' element={<Contact />} />
             <Route path='hotels' element={<Hotels />} />
             <Route path='details/:id' element={<Details />} />
+
             <Route path='admin' element={<Admin />} />
             <Route path='login' element={<Login />} />
           </Routes>
--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -1,7 +1,27 @@
+import AuthContext from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import LoginForm from "../forms/LoginForm";
+
+
 const Admin = () => {
+    const [auth, setAuth] = useContext(AuthContext);
+    const navigate = useNavigate();
+
+
+    if(auth === null) {
+        return (
+            <>
+                <LoginForm/>
+            </>
+        );
+    } else {
+
     return (
         <h1>Admin</h1>
     );
+    }
 };
 
 export default Admin;
