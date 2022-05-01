a18c550c70b339ea601533888c0ee5e0618cbe77
John HC <69842878+Purgatos81@users.noreply.github.com>
2022-05-01 13:54:21 +0200

    edit: removed unused elements

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -1,13 +1,10 @@
 import AuthContext from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import LoginForm from "../forms/LoginForm";
 
 
 const Admin = () => {
     const [auth, setAuth] = useContext(AuthContext);
-    const navigate = useNavigate();
 
 
     if(auth === null) {
